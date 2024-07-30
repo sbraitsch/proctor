8b74708d8c9bd4c8fa00593877cd9a0f99a2f0a5
Simon Braitsch <S.Braitsch@gmx.de>
2024-07-30 23:27:57 +0200

    fixed issues with early return by wrapping block in closure, made names unique in case of shadowing

--- src/lib.rs
+++ src/lib.rs
@@ -6,19 +6,19 @@ use syn::{parse_macro_input, ItemFn};
 #[proc_macro_attribute]
 pub fn timed(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemFn);
-    let func_name = &input.sig.ident;
-    let func_block = &input.block;
-    let func_sig = &input.sig;
+    let fn_name = &input.sig.ident;
+    let fn_block = &input.block;
+    let fn_sig = &input.sig;
 
     let expanded = quote! {
-        #func_sig {
+        #fn_sig {
             use std::time::{Instant, Duration};
-            let start = Instant::now();
-            let result = #func_block;
-            let elapsed = start.elapsed();
-
-            println!("Result of {} => {:?} after {:?}.", stringify!(#func_name), result, elapsed);
-            result
+            let __start = Instant::now();
+            let __result = (|| {
+                #fn_block
+            })();
+            println!("Result of {} => {:?} after {:?}.", stringify!(#fn_name), __result, __start.elapsed());
+            __result
        }
     };
 
